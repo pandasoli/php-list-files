693966af65adfc648368d5d4b05b51a4326cf2fd
pandasoli <sadpandasoli@gmail.com>
2023-02-02 20:25:28 -0300

    feat: added version 2

--- list.php
+++ list.php
@@ -70,159 +70,38 @@
 </head>
 <body>
     <div id='initial'>
-        <svg id='btnBack' onclick='javascript:history.go(-1)' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.com/svgjs' version='1.1' width='20' height='20' style='position: relative; display: inline-block; left: -60px;' viewBox='0 0 64 64' style='enable-background:new 0 0 512 512' xml:space='preserve' class=''><g><script>
-        try {
-            Object.defineProperty(navigator, 'globalPrivacyControl', {
-                value: false,
-                enumerable: true
-            })
-            // Remove script tag after execution
-            document.currentScript.parentElement.removeChild(document.currentScript)
-        } catch (e) {}</script><path xmlns='http://www.w3.org/2000/svg' d='m54 30h-39.899l15.278-14.552c.8-.762.831-2.028.069-2.828-.761-.799-2.027-.831-2.828-.069l-17.448 16.62c-.755.756-1.172 1.76-1.172 2.829 0 1.068.417 2.073 1.207 2.862l17.414 16.586c.387.369.883.552 1.379.552.528 0 1.056-.208 1.449-.621.762-.8.731-2.065-.069-2.827l-15.342-14.552h39.962c1.104 0 2-.896 2-2s-.896-2-2-2z' fill='#d2d2d2' data-original='#000000' style=''/><script>try {
-                        Object.defineProperty(screen, 'availTop', { value: 0 });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(screen, 'availLeft', { value: 0 });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(screen, 'availWidth', { value: 1366 });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(screen, 'availHeight', { value: 768 });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(screen, 'colorDepth', { value: 24 });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(screen, 'pixelDepth', { value: 24 });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(navigator, 'keyboard', { value: undefined });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(navigator, 'hardwareConcurrency', { value: 8 });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(navigator, 'deviceMemory', { value: 8 });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(navigator, 'webkitTemporaryStorage', { value: undefined });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(navigator, 'webkitPersistentStorage', { value: undefined });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(navigator, 'appVersion', { value: '5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' });
-                    } catch (e) {}
-                    try {
-                        Object.defineProperty(navigator, 'doNotTrack', { value: null });
-                    } catch (e) {}
-                    
-                navigator.getBattery = function getBattery () {
-                let battery = {'charging':true,'chargingTime':0,'dischargingTime':null,'level':1}
-            
-                try {
-                    Object.defineProperty(battery, 'onchargingchange', {
-                        enumerable: true,
-                        configurable: false,
-                        writable: false,
-                        value: undefined
-                    })
-                } catch (e) {}
-                
-                try {
-                    Object.defineProperty(battery, 'onchargingtimechange', {
-                        enumerable: true,
-                        configurable: false,
-                        writable: false,
-                        value: undefined
-                    })
-                } catch (e) {}
-                
-                try {
-                    Object.defineProperty(battery, 'ondischargingtimechange', {
-                        enumerable: true,
-                        configurable: false,
-                        writable: false,
-                        value: undefined
-                    })
-                } catch (e) {}
-                
-                try {
-                    Object.defineProperty(battery, 'onlevelchange', {
-                        enumerable: true,
-                        configurable: false,
-                        writable: false,
-                        value: undefined
-                    })
-                } catch (e) {}
-                
-                    battery.addEventListener = function addEventListener () {
-                        return
-                    }
-                
-                return Promise.resolve(battery)
-                }
-            
-            try {
-                window.screenY = 0
-            } catch (e) { }
-        
-            try {
-                window.screenTop = 0
-            } catch (e) { }
-        
-            try {
-                window.top.window.outerHeight = window.screen.height
-            } catch (e) { }
-        
-            try {
-                window.screenX = 0
-            } catch (e) { }
-        
-            try {
-                window.screenLeft = 0
-            } catch (e) { }
-        
-            try {
-                window.top.window.outerWidth = window.screen.width
-            } catch (e) { }
-        </script></g></svg>
+        <svg version='1.1' fill='#d2d2d2' id='btnBack'  onclick='javascript:history.go(-1)' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='20' height='20' style='position: relative; display: inline-block; left: -60px;' viewBox='0 0 512 512' style='enable-background:new 0 0 512 512;' xml:space='preserve'>
+            <g>
+                <path d='M492,236H68.442l70.164-69.824c7.829-7.792,7.859-20.455,0.067-28.284c-7.792-7.83-20.456-7.859-28.285-0.068l-104.504,104c-0.007,0.006-0.012,0.013-0.018,0.019c-7.809,7.792-7.834,20.496-0.002,28.314c0.007,0.006,0.012,0.013,0.018,0.019l104.504,104c7.828,7.79,20.492,7.763,28.285-0.068c7.792-7.829,7.762-20.492-0.067-28.284L68.442,276H492c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z'/>
+            </g>
+        </svg>
 
         <h1>List of php files in this folder</h1>
 
         <?php
             $dir = isset( $_GET['q'] ) ? $_GET['q'] : './';
             loadDirectory($dir, $dir);
-
-            function enterDirectory($folder) {
-                $dir += '/'. $folder. '/';
-                loadDirectory($dir, $dir);
-            }
             
             function loadDirectory($folder, $dir) {
+                $programname = basename(__FILE__);
                 $directory = dir($folder);
                 while(($file = $directory -> read()) != false)
                 {
-                    if ( $file != '.' and $file != '..' )
+                    if ( $file != '.' and $file != '..' and is_dir($dir. '/'. $file) )
                     {
-                        if (is_file($dir. '/'. $file)) {
-                            echo "<a href='http://localhost:8080/$dir/$file'";
-                        }
-                        else {
-                            echo "<a href='http://localhost:8080/list.php?q=$dir/$file'";
-                        }
+                        echo "<a href='http://localhost:8080/$programname?q=$dir/$file' style='background-color: rgba(100, 100, 100, .34);'> $file </a>";
+                    }
+                }
+                $directory -> close();
+
+                $directory = dir($folder);
+                while(($file = $directory -> read()) != false)
+                {
+                    if ( $file != '.' and $file != '..' and is_file($dir. '/'. $file) )
+                    {
+                        echo "<a href='http://localhost:8080/$dir/$file' style='background-color: rgba(0, 0, 0, .4);'> $file </a>";
     
-                        if (!is_dir($dir. '/'. $file)) {
-                            echo "style='background-color: rgba(0, 0, 0, .4);' ";
-                        }
-                        else {
-                            echo "style='background-color: rgba(100, 100, 100, .34);' ";
-                        }
-    
-                        echo "> $file </a>";
-    
-                        if ($file == basename(__FILE__)) {
+                        if ($file == $programname) {
                             echo "
                             <svg width='16px' version='1.1' id='Capa_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' style='display: block; position: relative; left: 31%; top: -25px; margin: 0px;' viewBox='0 0 511.999 511.999' style='enable-background:new 0 0 511.999 511.999;' xml:space='preserve'>
                             <path style='fill:#FFDC64;' d='M452.71,157.937l-133.741-12.404L265.843,22.17c-3.72-8.638-15.967-8.638-19.686,0l-53.126,123.362 L59.29,157.937c-9.365,0.868-13.149,12.516-6.084,18.723l100.908,88.646l-29.531,131.029c-2.068,9.175,7.841,16.373,15.927,11.572 L256,339.331l115.49,68.576c8.087,4.802,17.994-2.397,15.927-11.572l-29.532-131.029l100.909-88.646 C465.859,170.453,462.074,158.805,452.71,157.937z'/>
@@ -237,8 +116,8 @@
                         }
                     }
                 }
-    
                 $directory -> close();
+    
                 
             }
         ?>
